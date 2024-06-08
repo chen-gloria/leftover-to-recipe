e37297056e8ea68cc825a9d9c87b0c3ef1805676
Rohith Kyla <rk@q-ai.eu>
2024-06-08 14:07:28 +1000

    chore: update api and UI, add getIngrediets

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -51,73 +51,47 @@ class RecipeController extends AbstractController
 
         // $result = $this->openAIService->getIngredients($base64);
         
-        $result ='{"id": "chatcmpl-9XgKVhL68AVR0Fl1qLHLtBsuQ9E4t", "object": "chat.completion", "created": 1717814567, "model": "gpt-4o-2024-05-13", "choices": [ { "index": 0, "message": { "role": "assistant", "content": "```json\n{\n \"Ingredients\": [\n \"Green onions\",\n \"Green chili peppers\",\n \"Garlic\",\n \"Limes\",\n \"Ginger\",\n \"Basil\",\n \"Cooked chicken\",\n \"Coconut milk\",\n \"Black peppercorns\",\n \"Brown sugar\"\n ],\n \"Recipes\": [\n {\n \"IsHealthy\": true,\n \"Title\": \"Thai Basil Chicken Stir-Fry\",\n \"Ingredients\": [\n \"Green onions\",\n \"Green chili peppers\",\n \"Garlic\",\n \"Limes\",\n \"Ginger\",\n \"Basil\",\n \"Cooked chicken\",\n \"Coconut milk\",\n \"Black peppercorns\"\n ],\n \"Instructions\": [\n \"1. Slice the green onions, green chili peppers, and garlic.\",\n \"2. Grate the ginger.\",\n \"3. Heat a pan over medium heat and add a small amount of oil.\",\n \"4. Add the green onions, green chili peppers, garlic, and ginger to the pan and sauté until fragrant.\",\n \"5. Add the cooked chicken to the pan and stir-fry for a few minutes.\",\n \"6. Pour in the coconut milk and bring to a simmer.\",\n \"7. Add the basil leaves and black peppercorns, and cook for another 2-3 minutes.\",\n \"8. Squeeze lime juice over the dish before serving.\"\n ]\n },\n {\n \"IsHealthy\": false,\n \"Title\": \"Sweet and Spicy Chicken\",\n \"Ingredients\": [\n \"Green onions\",\n \"Green chili peppers\",\n \"Garlic\",\n \"Limes\",\n \"Ginger\",\n \"Cooked chicken\",\n \"Brown sugar\",\n \"Black peppercorns\"\n ],\n \"Instructions\": [\n \"1. Slice the green onions, green chili peppers, and garlic.\",\n \"2. Grate the ginger.\",\n \"3. Heat a pan over medium heat and add a small amount of oil.\",\n \"4. Add the green onions, green chili peppers, garlic, and ginger to the pan and sauté until fragrant.\",\n \"5. Add the cooked chicken to the pan and stir-fry for a few minutes.\",\n \"6. Sprinkle brown sugar over the chicken and stir well to coat.\",\n \"7. Add black peppercorns and cook for another 2-3 minutes until the sugar has caramelized.\",\n \"8. Squeeze lime juice over the dish before serving.\"\n ]\n }\n ]\n}\n```" }, "logprobs": null, "finish_reason": "stop" } ], "usage": { "prompt_tokens": 416, "completion_tokens": 526, "total_tokens": 942 }, "system_fingerprint": "fp_aa87380ac5" }';
+        // $result ='{"id": "chatcmpl-9XgKVhL68AVR0Fl1qLHLtBsuQ9E4t", "object": "chat.completion", "created": 1717814567, "model": "gpt-4o-2024-05-13", "choices": [ { "index": 0, "message": { "role": "assistant", "content": "```json\n{\n \"Ingredients\": [\n \"Green onions\",\n \"Green chili peppers\",\n \"Garlic\",\n \"Limes\",\n \"Ginger\",\n \"Basil\",\n \"Cooked chicken\",\n \"Coconut milk\",\n \"Black peppercorns\",\n \"Brown sugar\"\n ],\n \"Recipes\": [\n {\n \"IsHealthy\": true,\n \"Title\": \"Thai Basil Chicken Stir-Fry\",\n \"Ingredients\": [\n \"Green onions\",\n \"Green chili peppers\",\n \"Garlic\",\n \"Limes\",\n \"Ginger\",\n \"Basil\",\n \"Cooked chicken\",\n \"Coconut milk\",\n \"Black peppercorns\"\n ],\n \"Instructions\": [\n \"1. Slice the green onions, green chili peppers, and garlic.\",\n \"2. Grate the ginger.\",\n \"3. Heat a pan over medium heat and add a small amount of oil.\",\n \"4. Add the green onions, green chili peppers, garlic, and ginger to the pan and sauté until fragrant.\",\n \"5. Add the cooked chicken to the pan and stir-fry for a few minutes.\",\n \"6. Pour in the coconut milk and bring to a simmer.\",\n \"7. Add the basil leaves and black peppercorns, and cook for another 2-3 minutes.\",\n \"8. Squeeze lime juice over the dish before serving.\"\n ]\n },\n {\n \"IsHealthy\": false,\n \"Title\": \"Sweet and Spicy Chicken\",\n \"Ingredients\": [\n \"Green onions\",\n \"Green chili peppers\",\n \"Garlic\",\n \"Limes\",\n \"Ginger\",\n \"Cooked chicken\",\n \"Brown sugar\",\n \"Black peppercorns\"\n ],\n \"Instructions\": [\n \"1. Slice the green onions, green chili peppers, and garlic.\",\n \"2. Grate the ginger.\",\n \"3. Heat a pan over medium heat and add a small amount of oil.\",\n \"4. Add the green onions, green chili peppers, garlic, and ginger to the pan and sauté until fragrant.\",\n \"5. Add the cooked chicken to the pan and stir-fry for a few minutes.\",\n \"6. Sprinkle brown sugar over the chicken and stir well to coat.\",\n \"7. Add black peppercorns and cook for another 2-3 minutes until the sugar has caramelized.\",\n \"8. Squeeze lime juice over the dish before serving.\"\n ]\n }\n ]\n}\n```" }, "logprobs": null, "finish_reason": "stop" } ], "usage": { "prompt_tokens": 416, "completion_tokens": 526, "total_tokens": 942 }, "system_fingerprint": "fp_aa87380ac5" }';
+        $result = '{ "id": "chatcmpl-9XhU5rxA1IlAIrbIy6XhxZuTAD3Ol", "object": "chat.completion", "created": 1717819005, "model": "gpt-4o-2024-05-13", "choices": [ { "index": 0, "message": { "role": "assistant", "content": "```json\n{\n \"ingredients\": [\n \"green onions\",\n \"green chilies\",\n \"garlic\",\n \"limes\",\n \"ginger\",\n \"black peppercorns\",\n \"dried tamarind\",\n \"coconut milk\",\n \"basil leaves\",\n \"grilled chicken\"\n ]\n}\n```" }, "logprobs": null, "finish_reason": "stop" } ], "usage": { "prompt_tokens": 286, "completion_tokens": 72, "total_tokens": 358 }, "system_fingerprint": "fp_aa87380ac5" }';
         $resultBody = json_decode($result);
 
         // trim the following: ```json
 
-        
         $choicesContent = str_replace("```json", "", ($resultBody->choices)[0]->message->content);
         $choicesContent = str_replace("\n", "", $choicesContent);
         $choicesContent = str_replace("```", "", $choicesContent);
 
-        dd(json_decode($result), $choicesContent, json_decode($choicesContent));
+        // dd(json_decode($result), $choicesContent, json_decode($choicesContent));
+        
+        $json_ingrediets_list = json_decode($choicesContent);
+
+        // Add the ingredients list to the getRecipes method
+        // dd($json_ingrediets_list->ingredients);
+        $ingredients = $json_ingrediets_list->ingredients;
+        // $recipes = $this->openAIService->getRecipes(json_encode($json_ingrediets_list->ingredients));
+        // $recipesBody = json_decode($recipes);
+
+
+        // $choicesContent = str_replace("```json", "", ($recipesBody->choices)[0]->message->content);
+        // $choicesContent = str_replace("\n", "", $choicesContent);
+        // $choicesContent = str_replace("```", "", $choicesContent);
+
+        // dd(json_decode($recipes), $choicesContent, json_decode($choicesContent));
+
         // Have some steps...
         // AI Prompt...
         // name and picture
-        $ingredientNames = [
-            [
-                'name' => 'Apple',
-            ],
-            [
-                'name' => 'Pear',
-            ],
-            [
-                'name' => 'Banana',
-            ],
-            [
-                'name' => 'Tempeh',
-            ],
-            [
-                'name' => 'Donut',
-            ],
-            [
-                'name' => 'Bread'
-            ]
-        ];
 
-        $initialRecipe = (new Recipe())
-                        ->setTitle("My initial recipe")
-                        ->setDescription("dummy dummy")
-                        ->setPreparationTime(0)
-                        ->setCookingTime(0)
-                        ->setDifficultyLevel(1)
-                        ->setCategory('dummy category')
-                        ->setCreatedOnDatetime(new DateTime());
-
-        foreach ($ingredientNames as $ingredientName) {
-            $ingredient = (new Ingredient())
-                            ->setName($ingredientName['name'])
-                            ->setPicture("dummy");
-            
-            $initialRecipe->addIngredients($ingredient);
-                            
-            $this->em->persist($ingredient);
-            $this->em->persist($initialRecipe);
-        }
-
-        $this->em->flush();
         
         return $this->redirectToRoute('get_ingredients', [
-            'initial_recipe_id' => $initialRecipe->getId()
+            'ingredients' => $ingredients
         ]);
     }
 
-    #[Route('/ingredients/{initial_recipe_id}', name: 'get_ingredients', methods: ['GET'])]
-    public function getIngredients($initial_recipe_id): Response
+    #[Route('/ingredients', name: 'get_ingredients', methods: ['GET'])]
+    public function getIngredients(Request $request): Response
     {
-        $recipe = $this->doctrine->getRepository(Recipe::class)->findOneBy([ 'id' => $initial_recipe_id ]);
-        $ingredients = $recipe->getIngredients();
+        $ingredients = $request->get('ingredients');
 
         return $this->render('_step_3_generate_ingeredients.html.twig', [
             'ingredients' => $ingredients
--- src/Service/OpenAIService.php
+++ src/Service/OpenAIService.php
@@ -10,7 +10,7 @@ class OpenAIService
 {
     public function getIngredients($base64_image)
     {
-        $api_key = '';
+        $api_key = 'sk-proj-yWBgHL8vXd8PpFU9t4zeT3BlbkFJETKZx0RhaChMYlZxYvyn';
         $model = 'gpt-4o';
 
         $url = 'https://api.openai.com/v1/chat/completions';
@@ -23,19 +23,7 @@ class OpenAIService
                     'role' => 'user', 
                     'content' => [
                         ['type' => 'text', 'text' => <<<EOF
-                        Extract the ingrediets present in the image. Output the ingredients identified.
-
-                        Then propose recipes (along with the ingredients), return at least 2 recipies, one which is healthy, and one which is unhealthy.
-                        
-                        Return Dish Title, Ingredients and Instructions in JSON.
-
-                        For example:
-                        {
-                        "IsHealthy": true,
-                        "Title": "Tempeh Stir-Fry",
-                        "Ingredients": ["Tempeh", "Red chili peppers", ... ],
-                        "Instructions": ["1. Slice the tempeh into thin strips.", "2. Heat a generous amount of oil in a pan over medium-high heat and fry the tempeh until crispy and golden brown. Drain on paper towels."...]
-                        }
+                        Extract the ingrediets present in the image.
 
                         JSON:
                         EOF],
@@ -63,6 +51,71 @@ class OpenAIService
         curl_close($ch);
 
         echo $response;
+        return $response;
+    }
 
+    public function getRecipes($ingrediets_list)
+    {
+        $api_key = 'sk-proj-yWBgHL8vXd8PpFU9t4zeT3BlbkFJETKZx0RhaChMYlZxYvyn';
+        $model = 'gpt-4o';
+
+        $url = 'https://api.openai.com/v1/chat/completions';
+
+        $data = [
+            'model' => $model,
+            'messages' => [
+                ['role' => 'system', 'content' => 'You are a helpful assistant that responds in JSON'],
+                [
+                    'role' => 'user', 
+                    'content' => [
+                        ['type' => 'text', 'text' => <<<EOF
+                        Given the following ingredients list: $ingrediets_list
+
+                        Generate recipes (along with the ingredients), return at least 2 recipies, one which is healthy, and one which is unhealthy.
+                        
+                        Return Dish Title, Ingredients and Instructions in JSON.
+
+                        For example:
+                        {
+                            "IsHealthy": true,
+                            "Title": "Tempeh Stir-Fry",
+                            "Ingredients": ["Tempeh", "Red chili peppers", ... ],
+                            "Instructions": ["1. Slice the tempeh into thin strips.", "2. Heat a pan over medium heat and add a bit of oil.", "3. Add minced garlic, ginger, and lemongrass to the pan and sauté until fragrant."...],
+                            "Summary": "This dish is a healthy and delicious vegan stir-fry that is packed with flavor and nutrients."
+                        }
+                        {
+                            "IsHealthy": false,
+                            "Title": "Fried Tempeh with Spicy Peanut Sauce",
+                            "Ingredients": ["Tempeh", "Red chili peppers", ... ],
+                            "Instructions": ["1. Slice the tempeh into thin strips.", "2. Heat a generous amount of oil in a pan over medium-high heat and fry the tempeh until crispy and golden brown. Drain on paper towels."...],
+                            "Summary": "This dish is a delicious and indulgent vegan treat that is perfect for a special occasion or when you want to impress your guests."
+                            }
+
+                        JSON:
+                        EOF],
+                    ]
+                ]
+            ],
+            'temperature' => 0.0
+        ];
+
+        $payload = json_encode($data);
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Bearer ' . $api_key,
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($payload)
+        ]);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        echo $response;
+        return $response;
     }
 }
